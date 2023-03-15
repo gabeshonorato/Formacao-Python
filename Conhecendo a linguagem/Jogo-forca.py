ed68b30d3396cead8553eb4edcee6dd1be01b5e9
Gabriela Honorato <119624249+gabeshonorato@users.noreply.github.com>
2023-03-14 21:39:14 -0400

    Create Jogo-forca.py

---
+++ Conhecendo a linguagem/Jogo-forca.py
@@ -0,0 +1,20 @@
+# forca.py
+
+def jogar():
+    print("*********************************")
+    print("***Bem vindo ao jogo da Forca!***")
+    print("*********************************")
+
+    palavra_secreta = morango
+    enforcou = False
+    acertou = False
+
+    while (not False and not False):
+        print('jogando...')
+
+
+
+print("Fim do jogo")
+
+if(__name__ == "__main__"):
+    jogar()
